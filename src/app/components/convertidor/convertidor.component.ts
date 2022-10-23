cb1a1a2e36702a7005e79f7770a0f0cbb8c38fa8
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-23 17:46:46 -0300

    .

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -12,6 +12,29 @@ export class ConvertidorComponent implements OnInit {
     total: number = 0;
     monedas: string[] = ['USD','EUR','LIBRA'];
 
+    convertir(): void {
+        
+        if ( this.tengo === "USD"){
+            if ( this.quiero === "USD") this.total = this.cantidad;
+            if ( this.quiero === "EUR") this.total = this.cantidad * 0.84;
+            if ( this.quiero === "LIBRA") this.total = this.cantidad * 0.75;   
+        }
+
+        if ( this.tengo === "EUR"){
+            if ( this.quiero === "USD") this.total = this.cantidad * 1.2;
+            if ( this.quiero === "EUR") this.total = this.cantidad;
+            if ( this.quiero === "LIBRA") this.total = this.cantidad * 0.9;   
+        }
+
+        if ( this.tengo === "LIBRA"){
+            if ( this.quiero === "USD") this.total = this.cantidad * 1.33;
+            if ( this.quiero === "EUR") this.total = this.cantidad * 1.11;
+            if ( this.quiero === "LIBRA") this.total = this.cantidad;   
+        }
+
+    }
+
+
     constructor() { }
 
     ngOnInit(): void {
